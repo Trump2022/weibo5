c8f1e746609fe00122e32c60bcfe65c389278ed1
Trump2022 <qsw2012@outlook.com>
2022-04-04 06:20:43 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -59,4 +59,9 @@ class User extends Authenticatable
         //return "http://www.gravatar.com/avatar/$hash?s=$size";
         return "/avatar/$hash.jpg";
     }
+
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
 }
